7603ccdec99fb9ac6060edd45e5b233460c4b95d
Sanket Nitnaware <sanketn1892@gmail.com>
2023-04-21 14:52:41 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 require("./start/db")();
 
